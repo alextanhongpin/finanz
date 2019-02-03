7a810360670f77305feb96f31f560c7e4d7407fb
alextanhongpin <alextan220990@gmail.com>
2019-02-04 00:12:55 +0800

    chore: cleanup structure

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 from functools import partial
-from tornado.options import define, options
+from signal import SIGTERM, SIGINT
+from uuid import uuid4
+import dataclasses
 import json
 import logging
 import signal
 import time
-import tornado.ioloop
-import tornado.web
-import dataclasses
 
-define('port', default=5000, help='the application port')
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+from tornado.web import Application, RequestHandler
+import tornado
 
 from usecase.crud_entry.model import Expense
 
+define('port', default=5000, help='the application port')
 
-class MainHandler(tornado.web.RequestHandler):
+HEADER_X_REQUEST_ID = 'X-Request-ID'
+
+
+class MainHandler(RequestHandler):
+    SUPPORTED_METHODS = ['GET', 'POST']
+
+    def initialize(self):
+        pass
+
+    def prepare(self):
+        '''This is initialized before every requests.'''
+        request_id = self.request.headers.get(HEADER_X_REQUEST_ID)
+        if not request_id:
+            self.request.headers[HEADER_X_REQUEST_ID] = str(uuid4())
+
+    def set_default_headers(self):
+        self.set_header('Content-Type', 'application/json; charset=utf-8')
+
     def get(self):
-        exp = Expense(id='1', amount=100.0, name='hello')
-
+        request_id = self.request.headers.get(HEADER_X_REQUEST_ID)
+        data = {'id': '1', 'amount': 100.0, 'name': 'hello'}
+        exp = Expense(**data)
+        #  exp = Expense(id='1', amount=100.0, name='hello')
         res = dataclasses.asdict(exp)
+        res['request_id'] = request_id
         self.write(json.dumps(res))
 
     def post(self):
@@ -26,7 +50,7 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 def sig_handler(server, sig, frame):
-    instance = tornado.ioloop.IOLoop.instance()
+    instance = IOLoop.instance()
 
     async def shutdown():
         server.stop()
@@ -38,18 +62,18 @@ def sig_handler(server, sig, frame):
 
 
 def main():
-    tornado.options.parse_command_line()
-    app = tornado.web.Application([
+    parse_command_line()
+    app = Application([
         (r"/", MainHandler),
     ], debug=True)
-    server = tornado.httpserver.HTTPServer(app)
+    server = HTTPServer(app)
     server.listen(options.port)
 
-    signal.signal(signal.SIGTERM, partial(sig_handler, server))
-    signal.signal(signal.SIGINT, partial(sig_handler, server))
+    signal.signal(SIGTERM, partial(sig_handler, server))
+    signal.signal(SIGINT, partial(sig_handler, server))
 
     print(f'listening to port *:{options.port}. press ctrl + c to cancel')
-    tornado.ioloop.IOLoop.current().start()
+    IOLoop.current().start()
 
 
 if __name__ == '__main__':
